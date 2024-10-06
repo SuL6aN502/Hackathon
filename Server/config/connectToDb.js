91c87f9e2ff3f33b2dfaa1ecb336a31ed235ce84
sultam <gbj6420@gmail.com>
2024-10-06 15:40:24 +0300

    update and solve puchases

--- Server/config/connectToDb.js
+++ Server/config/connectToDb.js
@@ -4,6 +4,7 @@ async function conectToDb() {
     try{
         await mongoose.connect(process.env.DB);
         console.log("db is Connected");
+        
     }catch(err){
         console.log(err + "\ndb is not connected");
     }
